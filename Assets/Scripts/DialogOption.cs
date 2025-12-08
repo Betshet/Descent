37cd61ecdc469298c425c745c656f315eb16e432
Betshet <45632828+Betshet@users.noreply.github.com>
2025-12-08 16:55:05 +0100

    oops

--- Assets/Scripts/DialogOption.cs
+++ Assets/Scripts/DialogOption.cs
@@ -1,20 +1,16 @@
 using UnityEngine;
 
+public enum Portrait
+{
+    None,
+    Char1
+}
+
 [CreateAssetMenu(fileName = "DialogOption", menuName = "Scriptable Objects/DialogOption")]
 public class DialogOption : ScriptableObject
 {
-    public enum Portrait
-    {
-        None,
-        Char1
-    }
-
-    [CreateAssetMenu(fileName = "Dialog", menuName = "ScriptableObjects/Dialog")]
-    public class Dialog : ScriptableObject
-    {
-        public string action;
-        public string dialogText;
-        public string[] dialogChoices;
-        public Portrait portrait;
-    }
+    public string action;
+    public string dialogText;
+    public string[] dialogChoices;
+    public Portrait portrait;
 }
